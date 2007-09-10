aecd4b27cb749de9b9ec23773193cd2d0070381c
Niklas Sorensson <nik@chalmers.se>
2007-09-10 15:30:55 +0200

    Welcome to a new general GNU Make build system!

--- mtl/template.mk
+++ mtl/template.mk
@@ -5,16 +5,18 @@
 ##        "make d"  for a debug version (no optimizations).
 ##        "make"    for the standard version (optimized, but with debug information and assertions active)
 
-CSRCS     ?= $(wildcard *.C)
-CHDRS     ?= $(wildcard *.h)
-COBJS     ?= $(addsuffix .o, $(basename $(CSRCS)))
+PWD        = $(shell pwd)
+EXEC      ?= $(notdir $(PWD))
+
+CSRCS      = $(wildcard $(PWD)/*.C) 
+DSRCS      = $(foreach dir, $(DEPDIR), $(filter-out $(MROOT)/$(dir)/Main.C, $(wildcard $(MROOT)/$(dir)/*.C)))
+CHDRS      = $(wildcard $(PWD)/*.h)
+COBJS      = $(CSRCS:.C=.o) $(DSRCS:.C=.o)
 
 PCOBJS     = $(addsuffix p,  $(COBJS))
 DCOBJS     = $(addsuffix d,  $(COBJS))
 RCOBJS     = $(addsuffix r,  $(COBJS))
 
-EXEC      ?= $(notdir $(shell pwd))
-LIB       ?= $(EXEC)
 
 CXX       ?= g++
 CFLAGS    ?= -Wall
@@ -22,20 +24,20 @@ LFLAGS    ?= -Wall
 
 COPTIMIZE ?= -O3
 
-.PHONY : s p d r rs lib libd clean 
+CFLAGS    += $(foreach dir, $(DEPDIR), -I$(MROOT)/$(dir))
+
+.PHONY : s p d r rs clean 
 
 s:	$(EXEC)
 p:	$(EXEC)_profile
 d:	$(EXEC)_debug
 r:	$(EXEC)_release
 rs:	$(EXEC)_static
-lib:	lib$(LIB).a
-libd:	lib$(LIB)d.a
 
 ## Compile options
 %.o:			CFLAGS +=$(COPTIMIZE) -ggdb -D DEBUG
 %.op:			CFLAGS +=$(COPTIMIZE) -pg -ggdb -D NDEBUG
-%.od:			CFLAGS +=-O0 -ggdb -D DEBUG # -D INVARIANTS
+%.od:			CFLAGS +=-O0 -ggdb -D DEBUG
 %.or:			CFLAGS +=$(COPTIMIZE) -D NDEBUG
 
 ## Link options
@@ -52,39 +54,32 @@ $(EXEC)_debug:		$(DCOBJS)
 $(EXEC)_release:	$(RCOBJS)
 $(EXEC)_static:		$(RCOBJS)
 
-lib$(LIB).a:	$(filter-out Main.or, $(RCOBJS))
-lib$(LIB)d.a:	$(filter-out Main.od, $(DCOBJS))
-
-
 ## Build rule
 %.o %.op %.od %.or:	%.C
-	@echo Compiling: "$@ ( $< )"
+	@echo Compiling: $(subst $(MROOT)/,,$@)
 	@$(CXX) $(CFLAGS) -c -o $@ $<
 
 ## Linking rules (standard/profile/debug/release)
 $(EXEC) $(EXEC)_profile $(EXEC)_debug $(EXEC)_release $(EXEC)_static:
-	@echo Linking: "$@ ( $^ )"
+	@echo Linking: "$@ ( $(foreach f,$^,$(subst $(MROOT)/,,$f)) )"
 	@$(CXX) $^ $(LFLAGS) -o $@
-
-## Library rule
-lib$(LIB).a lib$(LIB)d.a:
-	@echo Library: "$@ ( $^ )"
-	@rm -f $@
-	@ar cq $@ $^
 
 ## Clean rule
 clean:
 	@rm -f $(EXEC) $(EXEC)_profile $(EXEC)_debug $(EXEC)_release $(EXEC)_static \
-	  $(COBJS) $(PCOBJS) $(DCOBJS) $(RCOBJS) *.core depend.mak lib$(LIB).a lib$(LIB)d.a
+	  $(COBJS) $(PCOBJS) $(DCOBJS) $(RCOBJS) *.core depend.mk 
 
 ## Make dependencies
 depend.mk: $(CSRCS) $(CHDRS)
-	@echo Making dependencies ...
-	@$(CXX) $(CFLAGS) -MM $(CSRCS) > depend.mk
-	@cp depend.mk /tmp/depend.mk.tmp
-	@sed "s/o:/op:/" /tmp/depend.mk.tmp >> depend.mk
-	@sed "s/o:/od:/" /tmp/depend.mk.tmp >> depend.mk
-	@sed "s/o:/or:/" /tmp/depend.mk.tmp >> depend.mk
-	@rm /tmp/depend.mk.tmp
+	@echo Making dependencies
+	@$(CXX) $(foreach dir, $(DEPDIR), -I$(MROOT)/$(dir)) \
+	   $(CSRCS) -MM | sed 's|\(.*\):|$(PWD)/\1 $(PWD)/\1r $(PWD)/\1d $(PWD)/\1p: \\\
+|' > depend.mk
+	@for dir in $(DEPDIR); do \
+	      if [ -r $(MROOT)/$${dir}/depend.mk ]; then \
+		  echo Depends on: $${dir}; \
+		  cat $(MROOT)/$${dir}/depend.mk >> depend.mk; \
+	      fi; \
+	  done
 
 -include depend.mk
