e543399d8efce0a5e947b1b17fc7d49da90a1887
Niklas Sorensson <nik@chalmers.se>
2007-09-25 14:44:29 +0200

    Changed copyright notices

--- core/Solver.C
+++ core/Solver.C
@@ -1,5 +1,6 @@
 /****************************************************************************************[Solver.C]
-MiniSat -- Copyright (c) 2003-2007, Niklas Een, Niklas Sorensson
+Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
+Copyright (c) 2007,      Niklas Sorensson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
