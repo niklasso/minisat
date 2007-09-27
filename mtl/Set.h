446a4eefe8bb914628abea422700433cc9c91f40
Niklas Sorensson <nik@chalmers.se>
2007-09-27 18:08:45 +0200

    First version of hash-set added
      This is completely untested! Expect bugfixes to be necessary. Also,
      a bug in Map.h was fixed.

--- mtl/Set.h
+++ mtl/Set.h
@@ -1,5 +1,5 @@
-/*******************************************************************************************[Map.h]
-Copyright (c) 2006-2007, Niklas Sorensson
+/*******************************************************************************************[Set.h]
+MiniSat -- Copyright (c) 2006-2007, Niklas Sorensson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -17,8 +17,8 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
-#ifndef Map_h
-#define Map_h
+#ifndef Set_h
+#define Set_h
 
 #include <stdint.h>
 
@@ -42,38 +42,36 @@ static const int nprimes          = 25;
 static const int primes [nprimes] = { 31, 73, 151, 313, 643, 1291, 2593, 5233, 10501, 21013, 42073, 84181, 168451, 337219, 674701, 1349473, 2699299, 5398891, 10798093, 21596719, 43193641, 86387383, 172775299, 345550609, 691101253 };
 
 //=================================================================================================
-// Hash table implementation of Maps
+// Hash Set implementation:
 //
 
-template<class K, class D, class H = Hash<K>, class E = Equal<K> >
-class Map {
-    struct Pair { K key; D data; };
+template<class T, class H = Hash<T>, class E = Equal<T> >
+class Set {
+    H       hash;
+    E       equals;
 
-    H          hash;
-    E          equals;
-
-    vec<Pair>* table;
-    int        cap;
-    int        size;
+    vec<T>* table;
+    int     cap;
+    int     size;
 
     // Don't allow copying (error prone):
-    Map<K,D,H,E>&  operator = (Map<K,D,H,E>& other) { assert(0); }
-                   Map        (Map<K,D,H,E>& other) { assert(0); }
+    Set<T,H,E>&  operator = (Set<T,H,E>& other) { assert(0); }
+                 Set        (Set<T,H,E>& other) { assert(0); }
 
-    int32_t index  (const K& k) const { return hash(k) % cap; }
-    void   _insert (const K& k, const D& d) { table[index(k)].push(); table[index(k)].last().key = k; table[index(k)].last().data = d; }
+    int32_t index  (const T& e) const { return hash(e) % cap; }
+    void   _insert (const T& e) { table[index(e)].push(e); }
     void    rehash () {
-        const vec<Pair>* old = table;
+        const vec<T>* old = table;
 
         int newsize = primes[0];
         for (int i = 1; newsize <= cap && i < nprimes; i++)
            newsize = primes[i];
 
-        table = new vec<Pair>[newsize];
+        table = new vec<T>[newsize];
 
         for (int i = 0; i < cap; i++){
             for (int j = 0; j < old[i].size(); j++){
-                _insert(old[i][j].key, old[i][j].data); }}
+                _insert(old[i][j]); }}
 
         delete [] old;
 
@@ -83,26 +81,26 @@ class Map {
     
     public:
 
-     Map () : table(NULL), cap(0), size(0) {}
-     Map (const H& h, const E& e) : hash(h), equals(e), table(NULL), cap(0), size(0){}
-    ~Map () { delete [] table; }
+     Set () : table(NULL), cap(0), size(0) {}
+     Set (const H& h, const E& e) : hash(h), equals(e), table(NULL), cap(0), size(0) {}
+    ~Set () { delete [] table; }
 
-    void insert (const K& k, const D& d) { if (size+1 > cap / 2) rehash(); _insert(k, d); size++; }
-    bool peek   (const K& k, D& d) {
+    void insert (const T& e) { if (size+1 > cap / 2) rehash(); _insert(e); size++; }
+    bool peek   (T& e) {
         if (size == 0) return false;
-        const vec<Pair>& ps = table[index(k)];
+        const vec<T>& ps = table[index(e)];
         for (int i = 0; i < ps.size(); i++)
-            if (equals(ps[i].key, k)){
-                d = ps[i].data;
+            if (equals(ps[i], e)){
+                e = ps[i];
                 return true; } 
         return false;
     }
 
-    void remove (const K& k) {
+    void remove (const T& e) {
         assert(table != NULL);
-        vec<Pair>& ps = table[index(k)];
+        vec<T>& ps = table[index(e)];
         int j = 0;
-        for (; j < ps.size() && !equals(ps[j].key, k); j++);
+        for (; j < ps.size() && !equals(ps[j], e); j++);
         assert(j < ps.size());
         ps[j] = ps.last();
         ps.pop();
