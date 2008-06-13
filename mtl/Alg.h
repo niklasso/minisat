73ff760ec91fcb2ff5b12f48ae6b244022f6149c
Niklas Sorensson <nik@chalmers.se>
2008-06-13 15:10:36 +0200

    Added a useful copy-procedure which works for nested vector types.

--- mtl/Alg.h
+++ mtl/Alg.h
@@ -21,10 +21,16 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #ifndef Minisat_Alg_h
 #define Minisat_Alg_h
 
+#include "mtl/Vec.h"
+
+namespace Minisat {
+
 //=================================================================================================
 // Useful functions on vector-like types:
 
-namespace Minisat {
+//=================================================================================================
+// Removing and searching for elements:
+//
 
 template<class V, class T>
 static inline void remove(V& ts, const T& t)
@@ -45,6 +51,32 @@ static inline bool find(V& ts, const T& t)
     return j < ts.size();
 }
 
+
+//=================================================================================================
+// Copying vectors with support for nested vector types:
+//
+
+// Base case:
+template<class T>
+static inline void copy(const T& from, T& to)
+{
+    to = from;
+}
+
+// Recursive case:
+template<class T>
+static inline void copy(const vec<T>& from, vec<T>& to, bool append = false)
+{
+    if (!append)
+        to.clear();
+    for (int i = 0; i < from.size(); i++){
+        to.push();
+        copy(from[i], to.last());
+    }
+}
+
+template<class T>
+static inline void append(const vec<T>& from, vec<T>& to){ copy(from, to, true); }
 
 };
 
