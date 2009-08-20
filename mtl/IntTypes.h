a8e6554f6b726dd165c00f440148055288b67284
Niklas Sorensson <niklasso@gmail.com>
2009-08-20 17:11:16 +0200

    More Solaris fixes.

--- mtl/IntTypes.h
+++ mtl/IntTypes.h
@@ -22,37 +22,37 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 // NOTE: __STDC_VERSION__ >= 199901L only seems to work for compiling pure C.
 
-#if __GNUC__
+#if defined(__GNUC__) && not defined(__sun)
     // GCC allows use of C99-standard headers also in C++:
-    
+
 #   define __STDC_LIMIT_MACROS
 #   define __STDC_FORMAT_MACROS
-    
+
 #   include <stdint.h>
 #   include <inttypes.h>
-    
+
 #   undef __STDC_LIMIT_MACROS
 #   undef __STDC_FORMAT_MACROS
-    
-    
+
+
 #else
     // Workarounds for other compilers:
-    
-#   ifdef __SUNPRO_CC
+
+#   ifdef __sun
         // Not sure if there are newer versions that support C99 headers. The
         // needed features are implemented in the headers below though:
-        
+
 #       include <sys/int_types.h>
 #       include <sys/int_fmtio.h>
 #       include <sys/int_limits.h>
-        
+
 #   else
         // NOTE: Add support for other compilers here.
 
 #       error "Unsupported compiler: needs support for C99-style int-types."
-        
+
 #   endif
-    
+
 #endif
 
 //=================================================================================================
