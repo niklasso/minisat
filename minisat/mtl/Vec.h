6820591ae0bdba3be170ca1f48d389ebb410bb1e
Niklas Sorensson <niklasso@gmail.com>
2011-09-22 14:02:44 +0200

    Revert to using copy constructor in Vec::push() rather than assignment.

--- minisat/mtl/Vec.h
+++ minisat/mtl/Vec.h
@@ -72,7 +72,8 @@ public:
 
     // Stack interface:
     void     push  (void)              { if (sz == cap) capacity(sz+1); new (&data[sz]) T(); sz++; }
-    void     push  (const T& elem)     { if (sz == cap) capacity(sz+1); data[sz++] = elem; }
+    //void     push  (const T& elem)     { if (sz == cap) capacity(sz+1); data[sz++] = elem; }
+    void     push  (const T& elem)     { if (sz == cap) capacity(sz+1); new (&data[sz++]) T(elem); }
     void     push_ (const T& elem)     { assert(sz < cap); data[sz++] = elem; }
     void     pop   (void)              { assert(sz > 0); sz--, data[sz].~T(); }
     // NOTE: it seems possible that overflow can happen in the 'sz+1' expression of 'push()', but
