555015ffe4220aaf3b87ab4172ee2639b3708459
Jeroen Bransen <J.Bransen@uu.nl>
2015-07-03 12:08:32 +0200

    Move StreamBuffer to separate file (such that only main depends on zlib)

--- minisat/core/Dimacs.h
+++ minisat/core/Dimacs.h
@@ -24,6 +24,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <stdio.h>
 
 #include "minisat/utils/ParseUtils.h"
+#include "minisat/utils/StreamBuffer.h"
 #include "minisat/core/SolverTypes.h"
 
 namespace Minisat {
