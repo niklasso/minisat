2e15880ea5b1c6343651d068b3e88b9a22cc15ab
Norbert Manthey <nmanthey@conp-solutions.com>
2019-04-11 23:50:34 +0200

    ci: test starexec
    We want to test whether the starexec package is stable, also during
    builds.

--- tools/ci.sh
+++ tools/ci.sh
@@ -19,5 +19,36 @@ echo "Solve and check 100 formulas with a 5s timeout"
 STATUS=0
 ./fuzz-drat.sh 100 5 || STATUS=$?
 
+popd
+CHECKERDIR=$(readlink -e tools/checker)
+
+# check starexec build
+# trap 'rm -rf $TMPD' EXIT
+TMPD=$(mktemp -d)
+
+# build starexec package, also for Sparrow2MergeSAT, currently based on local code
+./tools/make-starexec.sh -r ~/git/riss-public -s ~/git/Sparrow
+
+# build starexec build
+MERGEZIP=$(ls MergeSAT*.zip | sort -V | tail -n 1)
+mv "$MERGEZIP" "$TMPD"
+pushd $TMPD
+
+mkdir -p ci_starexec_tmpdir
+STAREXEC_TMPDIR=$(readlink -e ci_starexec_tmpdir)
+
+unzip "$MERGEZIP"
+./starexec_build
+TARGETS=$(ls ./bin/starexec_run_*)
+
+# fuzz each of the starexec targets
+for target in $TARGETS
+do
+	FULL_TARGET=$(readlink -e $target)
+	pushd $CHECKERDIR
+	FUZZ_SOLVER="$FULL_TARGET" DEFAULT_MERGESAT_TMPDIR="$STAREXEC_TMPDIR" ./fuzz-drat.sh 10 5
+	popd
+done
+
 # Forward exit status from fuzzing
 exit $STATUS
