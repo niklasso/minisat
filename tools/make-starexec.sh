fa47836584697c7f6dba5a578e41a1b4f2d29c42
Norbert Manthey <nmanthey@conp-solutions.com>
2019-04-11 22:12:39 +0200

    starexec: add dependencies
    SparrowToMergeSAT requires Sparrow as well as Coprocessor. This change
    allows to specify where the tools should be taken from when creating
    the package for starexec. Furthermore, the build script can now handle
    a Riss and Sparrow directory.

--- tools/make-starexec.sh
+++ tools/make-starexec.sh
@@ -1,10 +1,105 @@
 #!/bin/bash
-# make-starexec.sh, Norbert Manthey, 2018
+# make-starexec.sh, Norbert Manthey, 2019
 #
 # build the starexec package from the current git branch
 
 # make sure we notice failures early
 set -e -x
+
+get_riss ()
+{
+	local arg="$1"
+	local -r CALLDIR=$(pwd)  # we need to store the package here
+
+	# is $arg a directory (to riss)?
+	if [ -d "$arg" ]
+	then
+		pushd "$arg"
+
+		# build the solver and select the latest version
+		./scripts/make-starexec.sh
+		RISSZIP=$(ls Riss*.zip | sort -V | tail -n 1)
+
+		# move Riss zip file
+		mkdir -p "$CALLDIR"/Riss
+		mv $RISSZIP "$CALLDIR"/Riss/Riss.zip
+
+		popd
+		return 0
+	fi
+
+	# is $arg a commit ID to clone from github?
+	git ls-remote https://github.com/conp-solutions/riss.git "$arg" || true
+	COMMIT=$(git ls-remote https://github.com/conp-solutions/riss.git "$arg" | awk '{print $1}')
+	[ -n "$COMMIT" ] || return
+
+	mkdir -p "$CALLDIR"/Riss-clone
+	pushd "$CALLDIR"/Riss-clone
+
+	git clone https://github.com/conp-solutions/riss.git
+	cd riss
+	git checkout -b "mergesat-build" "$COMMIT"
+
+	# build the solver and select the latest version
+	./scripts/make-starexec.sh
+	RISSZIP=$(ls Riss*.zip | sort -V | tail -n 1)
+
+	# move Riss zip file
+	mkdir -p "$CALLDIR"/Riss
+	mv $RISSZIP "$CALLDIR"/Riss/Riss.zip
+
+	popd
+	# clean up cloned solver
+	rm -rf "$CALLDIR"/Riss-clone
+}
+
+get_sparrow ()
+{
+	local arg="$1"
+	local SPARROWDIR=
+	local -r CALLDIR=$(pwd)  # we need to store the package here
+
+	# is $arg a directory (to sparrow)?
+	if [ -d "$arg" ]
+	then
+		pushd "$arg"
+
+		# clean the solver and zip it
+		make clean
+		zip -r -y -9 Sparrow.zip *
+
+		# move Sparrow zip file
+		mkdir -p "$CALLDIR"/Sparrow
+		mv Sparrow.zip "$CALLDIR"/Sparrow/Sparrow.zip
+
+		popd
+		return 0
+	fi
+
+	# is $arg a commit ID to clone from github?
+	git ls-remote https://github.com/adrianopolus/Sparrow.git "$arg" || true
+	COMMIT=$(git ls-remote https://github.com/adrianopolus/Sparrow.git "$arg" | awk '{print $1}')
+	[ -n "$COMMIT" ] || return
+
+	mkdir -p "$CALLDIR"/Sparrow-clone
+	pushd "$CALLDIR"/Sparrow-clone
+
+	git clone https://github.com/adrianopolus/Sparrow.git
+	cd Sparrow
+	git checkout -b "mergesat-build" "$COMMIT"
+
+	# clean the solver and zip it
+	make clean
+	zip -r -y -9 Sparrow.zip *
+
+	# move Sparrow zip file
+	mkdir -p "$CALLDIR"/Sparrow
+	mv Sparrow.zip "$CALLDIR"/Sparrow/Sparrow.zip
+
+	popd
+	# clean up cloned solver
+	rm -rf "$CALLDIR"/Sparrow-clone
+}
 
 # make sure we know where the code is
 SOLVERDIR=$(pwd)
@@ -23,6 +118,24 @@ then
 	exit 1
 fi
 
+RISSOPT=""
+SPARROWOPT=""
+# do we want to package Riss(for Coprocessor) or Sparrow as well?
+while getopts "r:s:" OPTION; do
+    case $OPTION in
+    r)
+        RISSOPT="$OPTARG"
+        ;;
+    s)
+        SPARROWOPT="$OPTARG"
+        ;;
+    *)
+        echo "Unknown options provided"
+        ;;
+    esac
+done
+
+
 # make sure we clean up
 trap 'rm -rf $TMPD' EXIT
 TMPD=$(mktemp -d)
@@ -40,6 +153,10 @@ git checkout $BRANCH
 git gc
 git prune
 popd
+
+# get the other packages?
+[ -z "$RISSOPT" ] || get_riss "$RISSOPT"
+[ -z "$SPARROWOPT" ] || get_sparrow "$SPARROWOPT"
 
 # compress
 zip -r -y -9 Minisat.zip *
