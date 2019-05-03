1c60e8d7352725f19d07b341b080c48c0b9d1ff3
Norbert Manthey <nmanthey@conp-solutions.com>
2019-05-04 00:24:06 +0200

    vec: add reverse method

--- minisat/mtl/Vec.h
+++ minisat/mtl/Vec.h
@@ -89,6 +89,9 @@ public:
     // Duplicatation (preferred instead):
     void copyTo(vec<T>& copy) const { copy.clear(); copy.growTo(sz); for (int i = 0; i < sz; i++) copy[i] = data[i]; }
     void moveTo(vec<T>& dest) { dest.clear(true); dest.data = data; dest.sz = sz; dest.cap = cap; data = NULL; sz = 0; cap = 0; }
+
+    /// reverse the order of the literals in the clause. take care when the clause is currently watched!
+    void reverse() { int i = 0, j = size() - 1; while (i < j) {const T tmp = data[i]; data[i] = data[j]; data[j] = tmp; ++i; --j;} }
 };
 
 
