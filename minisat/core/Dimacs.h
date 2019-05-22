ab1a1f366990d1c0196bab3517f61ff595ebcb8f
Mate Soos <soos.mate@gmail.com>
2019-05-22 20:32:04 +0200

    Removing warning

--- minisat/core/Dimacs.h
+++ minisat/core/Dimacs.h
@@ -47,7 +47,7 @@ static void readClause(B& in, Solver& S, vec<Lit>& lits) {
 template<class B, class Solver>
 static void parse_DIMACS_main(B& in, Solver& S, bool strictp = false) {
     vec<Lit> lits;
-    int vars    = 0;
+    //int vars    = 0;
     int clauses = 0;
     int cnt     = 0;
     for (;;){
@@ -55,7 +55,7 @@ static void parse_DIMACS_main(B& in, Solver& S, bool strictp = false) {
         if (*in == EOF) break;
         else if (*in == 'p'){
             if (eagerMatch(in, "p cnf")){
-                vars    = parseInt(in);
+                parseInt(in);
                 clauses = parseInt(in);
                 // SATRACE'06 hack
                 // if (clauses > 4000000)
