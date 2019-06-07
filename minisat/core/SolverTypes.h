7eb6015313561a2586032574788fcb133eeaa19f
Jordi Petit <jpetit@cs.upc.edu>
2019-06-07 14:05:44 +0200

    Fix compilation error
    CLang reports this error:
    
    ./minisat/core/SolverTypes.h:55:16: error: friend declaration specifying a default argument must be a definition
        friend Lit mkLit(Var var, bool sign = false);
                   ^
    ./minisat/core/SolverTypes.h:63:14: error: friend declaration specifying a default argument must be the only declaration
    inline  Lit  mkLit     (Var var, bool sign) { Lit p; p.x = var + var + (int)sign; return p; }
                 ^
    ./minisat/core/SolverTypes.h:55:16: note: previous declaration is here
        friend Lit mkLit(Var var, bool sign = false);
                   ^
    2 errors generated.

--- minisat/core/SolverTypes.h
+++ minisat/core/SolverTypes.h
@@ -52,7 +52,8 @@ struct Lit {
     int     x;
 
     // Use this as a constructor:
-    friend Lit mkLit(Var var, bool sign = false);
+    friend Lit mkLit(Var var, bool sign);
+    friend Lit mkLit(Var var);
 
     bool operator == (Lit p) const { return x == p.x; }
     bool operator != (Lit p) const { return x != p.x; }
@@ -61,6 +62,7 @@ struct Lit {
 
 
 inline  Lit  mkLit     (Var var, bool sign) { Lit p; p.x = var + var + (int)sign; return p; }
+inline  Lit  mkLit     (Var var)            { return mkLit(var, false); }
 inline  Lit  operator ~(Lit p)              { Lit q; q.x = p.x ^ 1; return q; }
 inline  Lit  operator ^(Lit p, bool b)      { Lit q; q.x = p.x ^ (unsigned int)b; return q; }
 inline  bool sign      (Lit p)              { return p.x & 1; }
