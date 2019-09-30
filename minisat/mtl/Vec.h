29a0273e06275844131d4ce6a127ee04b5ad57d9
Norbert Manthey <nmanthey@conp-solutions.com>
2019-09-30 21:15:44 +0200

    vec: make sure to not fail in realloc
    The original code did not store the old memory when calling a
    potentially failing realloc, and furthermore already increased the
    capacity before evaluating the failure.
    
    To be able to catch corner cases, split allocation and evaluation,
    so that errors can be handled properly.

--- minisat/mtl/Vec.h
+++ minisat/mtl/Vec.h
@@ -99,8 +99,14 @@ template<class T>
 void vec<T>::capacity(int min_cap) {
     if (cap >= min_cap) return;
     int add = imax((min_cap - cap + 1) & ~1, ((cap >> 1) + 2) & ~1);   // NOTE: grow by approximately 3/2
-    if (add > INT_MAX - cap || ((data = (T*)::realloc(data, (cap += add) * sizeof(T))) == NULL) && errno == ENOMEM)
+    if (add > INT_MAX - cap) throw OutOfMemoryException();
+
+    T* newdata = (T*)::realloc(data, (cap + add) * sizeof(T));
+    if(newdata == NULL && errno == ENOMEM)
         throw OutOfMemoryException();
+
+    cap += add;
+    data = newdata;
  }
 
 
