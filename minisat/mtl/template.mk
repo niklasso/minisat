2c4beff6b48107d72865ca66ac0fca5170c5aeb5
Norbert Manthey <nmanthey@conp-solutions.com>
2020-03-07 23:26:17 +0100

    build: use C++11

--- minisat/mtl/template.mk
+++ minisat/mtl/template.mk
@@ -19,7 +19,7 @@ RCOBJS     = $(addsuffix r,  $(COBJS))
 
 
 CXX       ?= g++
-CFLAGS    ?= -Wall -Wno-parentheses
+CFLAGS    ?= -Wall -Wno-parentheses -std=c++11
 LFLAGS    ?= -Wall
 
 COPTIMIZE ?= -O3
