niklasso/minisat

commit 7404f173e61dd2cb37819467e4fee0e3e4aa9578

decision: toggle more stable
When toggling decision heuristics without updating decision heaps is okay
as long as the solver is not used in an incremental mode.

In incremental mode however, we might solve a formula, and call the solver
again afterwards. This results in switched back to VSIDS, eventually
without rebuilding the heap fully. This can lead to reporting a satisfiable
answer for unsatisfiable formulas, as has happened when using a buggy
implementation as a SAT backend for the OpenWBO SAT solver.

This commit introduced a method that allows to toggle the decision heaps
without running into this problem.

minisat/core/Solver.cc
@@ -2100,6 +2100,16 @@ static double luby(double y, int x)
     return pow(y, seq);
 }
 
+void Solver::toggle_decision_heuristic(bool to_VSIDS)
+{
+    if (to_VSIDS) { // initialize VSIDS heap again?
+        order_heap_VSIDS.build(DISTANCE ? order_heap_distance.elements() : order_heap_CHB.elements());
+    } else {
+        order_heap_distance.build(order_heap_VSIDS.elements());
+        order_heap_CHB.build(order_heap_VSIDS.elements());
+    }
+}
+
 // NOTE: assumptions passed in member-variable 'assumptions'.
 lbool Solver::solve_()
 {
@@ -2126,19 +2136,14 @@ lbool Solver::solve_()
 
     add_tmp.clear();
 
+    // toggle back to VSIDS
+    if (!VSIDS) toggle_decision_heuristic(true);
     VSIDS = true;
     int init = 10000;
-    if (order_heap_VSIDS.size() != order_heap_distance.size() || order_heap_VSIDS.size() != order_heap_CHB.size()) {
-        order_heap_VSIDS.build(DISTANCE ? order_heap_distance.elements() : order_heap_CHB.elements());
-    }
     while (status == l_Undef && init > 0 && withinBudget()) status = search(init);
     VSIDS = false;
-    if (status == l_Undef) {
-        if (order_heap_VSIDS.size() != order_heap_distance.size() || order_heap_VSIDS.size() != order_heap_CHB.size()) {
-            Heap<VarOrderLt> &order_heap = DISTANCE ? order_heap_distance : order_heap_CHB;
-            order_heap.build(order_heap_VSIDS.elements());
-        }
-    }
+    // do not use VSIDS now
+    toggle_decision_heuristic(false);
 
     // Search:
     uint64_t curr_props = 0;
@@ -2168,12 +2173,7 @@ lbool Solver::solve_()
                 printf("c Switched to LRB/DISTANCE.\n");
             }
 
-            if (VSIDS) { // initialize VSIDS heap again?
-                order_heap_VSIDS.build(DISTANCE ? order_heap_distance.elements() : order_heap_CHB.elements());
-            } else {
-                order_heap_distance.build(order_heap_VSIDS.elements());
-                order_heap_CHB.build(order_heap_VSIDS.elements());
-            }
+            toggle_decision_heuristic(true);
 
             fflush(stdout);
             picked.clear();

minisat/core/Solver.h
@@ -398,7 +398,8 @@ class Solver
     void removeSatisfied(vec<CRef> &cs); // Shrink 'cs' to contain only non-satisfied clauses.
     void safeRemoveSatisfied(vec<CRef> &cs, unsigned valid_mark);
     void rebuildOrderHeap();
-    bool binResMinimize(vec<Lit> &out_learnt); // Further learnt clause minimization by binary resolution.
+    bool binResMinimize(vec<Lit> &out_learnt);     // Further learnt clause minimization by binary resolution.
+    void toggle_decision_heuristic(bool to_VSIDS); // Switch between decision heuristic heaps.
 
     // Maintaining Variable/Clause activity:
     //

commit e5ae2e325703aa3d3241e3f8db1ba21835c13853

decision: fix toggling heaps
Until now, we always moved heaps to VSIDS, independently of which heap
is actually used for picking decision variables. In the worst case, this
results in the solver reporting SAT for unsatisfiable formulas, namely
in combination with partial restarts, where the decision level is not
zero and there are pending assigned variables.

Fixes: 7404f17 (decision: toggle more stable)

Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

minisat/core/Solver.cc
@@ -2237,6 +2237,7 @@ lbool Solver::solve_()
         if (switch_mode) {
             switch_mode = false;
             VSIDS = !VSIDS;
+            toggle_decision_heuristic(VSIDS); // switch to VSIDS
             if (verbosity >= 1) {
                 if (VSIDS) {
                     printf("c Switched to VSIDS.\n");
@@ -2244,9 +2245,6 @@ lbool Solver::solve_()
                     printf("c Switched to LRB/DISTANCE.\n");
                 }
             }
-
-            toggle_decision_heuristic(true);
-
             fflush(stdout);
         }
     }