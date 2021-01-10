a69c5dd4638ea4fb6315f9ab575e0b19e49893d8
Norbert Manthey <nmanthey@conp-solutions.com>
2021-01-10 14:41:59 +0100

    ci,configurations: split release and debug
    For debug builds, test execution is costly. Hence, we should reduce the
    number of formulas to be tested per configuration.
    
    This change uses the number of formulas to be tested per tool
    configuration. While being at it, we also fix the style of the script
    using:
    
    shfmt -i 4 -w <script>
    
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- tools/checker/fuzz-check-configurations.sh
+++ tools/checker/fuzz-check-configurations.sh
@@ -4,33 +4,37 @@
 # formulas.
 
 # Formulas to test per iteration
-declare -i FORMULA_PER_CONFIG=2500
+declare -i FORMULA_PER_CONFIG_RELEASE=3000
+declare -i FORMULA_PER_CONFIG_DEBUG=500
 
 # List of configurations to consider
 CONFIGURATIONS=("../../build/release/bin/mergesat"
-                "../../build/debug/bin/mergesat -check-sat"
-                "../../build/debug/bin/mergesat -check-sat -no-pre"
-                "../../build/debug/bin/mergesat -check-sat -rtype=4 -VSIDS-lim=50 -VSIDS-init-lim=50"
-                "../../build/debug/bin/mergesat -check-sat -verb=2"
-                "../../build/debug/bin/mergesat -check-sat -check-proof=1 -drup-file=proof.drat")
+    "../../build/debug/bin/mergesat -check-sat"
+    "../../build/debug/bin/mergesat -check-sat -no-pre"
+    "../../build/debug/bin/mergesat -check-sat -rtype=4 -VSIDS-lim=50 -VSIDS-init-lim=50"
+    "../../build/debug/bin/mergesat -check-sat -verb=2"
+    "../../build/debug/bin/mergesat -check-sat -check-proof=1 -drup-file=proof.drat")
 
 # Current directory
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "$SCRIPT_DIR"
 
 declare -i overall_status=0
-for CONFIG in "${CONFIGURATIONS[@]}"
-do
+for CONFIG in "${CONFIGURATIONS[@]}"; do
     echo "test configuration: $CONFIG"
-    
-    echo "$CONFIG \$1" > check-solver-config.sh
+
+    echo "$CONFIG \$1" >check-solver-config.sh
     chmod u+x check-solver-config.sh
-    
+
     status=0
+
+    # select number of formulas to test based on configuration
+    FORMULA_PER_CONFIG="$FORMULA_PER_CONFIG_RELEASE"
+    grep -q "debug" check-solver-config.sh && FORMULA_PER_CONFIG="$FORMULA_PER_CONFIG_DEBUG"
+
     ./fuzzcheck.sh ./check-solver-config.sh "$FORMULA_PER_CONFIG" || status=$?
-    if [ "$status" -ne 0 ]
-    then
+    if [ "$status" -ne 0 ]; then
         echo "configuration failed!"
         overall_status=$status
     fi
