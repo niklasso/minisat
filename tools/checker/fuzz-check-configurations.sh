d5a059766b8a1ad98b7d8f18a84dd83ffe34472a
Norbert Manthey <nmanthey@conp-solutions.com>
2021-01-14 23:57:59 +0100

    ci: add no-pre configuration
    Some issues can be simplified further with the delta debugger if no
    simplification is used. Hence, also test this case.
    
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- tools/checker/fuzz-check-configurations.sh
+++ tools/checker/fuzz-check-configurations.sh
@@ -10,6 +10,7 @@ declare -i FORMULA_PER_CONFIG_DEBUG=400
 # List of configurations to consider
 CONFIGURATIONS=("../../build/release/bin/mergesat"
     "../../build/debug/bin/mergesat -check-sat -check-proof=1 -cpu-lim=4 -drup-file=proof.drat -verb=2 -VSIDS-init-lim=100 -lcm-delay=10 -lcm-delay-inc=10 -rfirst=2"
+    "../../build/debug/bin/mergesat -check-sat -check-proof=1 -cpu-lim=4 -drup-file=proof.drat -verb=2 -no-pre -VSIDS-init-lim=100 -lcm-delay=10 -lcm-delay-inc=10 -rfirst=2"
     "../../build/debug/bin/mergesat -check-sat"
     "../../build/debug/bin/mergesat -check-sat -no-pre"
     "../../build/debug/bin/mergesat -check-sat -rtype=4 -VSIDS-lim=50 -VSIDS-init-lim=50"
