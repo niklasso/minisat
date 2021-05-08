576c9560dc846d20df48cd4922933f123d517e41
Norbert Manthey <nmanthey@conp-solutions.com>
2021-05-09 00:22:41 +0200

    diversify: set default to 0
    Make sure we use the best default we can.
    
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- minisat/simp/Main.cc
+++ minisat/simp/Main.cc
@@ -135,7 +135,7 @@ int main(int argc, char **argv)
         StringOption pcs_file("MAIN", "pcs-file", "Print solver parameter configuration to this file.", "");
 
         IntOption opt_diversify_rank("MAIN", "diversify-rank", "Select a diversification rank to quickly test another configuration",
-                                     INT32_MAX, IntRange(-1, INT32_MAX));
+                                     0, IntRange(-1, INT32_MAX));
         IntOption opt_diversify_size("MAIN", "diversify-size", "Select a diversification size to quickly test another configuration",
                                      32, IntRange(1, INT32_MAX));
 
