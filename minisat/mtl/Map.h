1bca8fb24796800ac6573805918b2bfd3427a556
Norbert Manthey <nmanthey@conp-solutions.com>
2021-05-22 22:56:45 +0200

    map: disable copy construction
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- minisat/mtl/Map.h
+++ minisat/mtl/Map.h
@@ -79,8 +79,9 @@ template <class K, class D, class H = Hash<K>, class E = Equal<K>> class Map
     int size;
 
     // Don't allow copying (error prone):
-    Map<K, D, H, E> &operator=(Map<K, D, H, E> &other) { assert(0); }
-    Map(Map<K, D, H, E> &other) { assert(0); }
+    Map<K, D, H, E> &operator=(Map<K, D, H, E> &other) = delete;
+    Map(Map<K, D, H, E> &other) = delete;
+    Map(Map<K, D, H, E> &&other) = delete;
 
     bool checkCap(int new_size) const { return new_size > cap; }
 
